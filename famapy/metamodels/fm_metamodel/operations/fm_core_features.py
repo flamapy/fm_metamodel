7784d75b8708c5bb337e09e13b3af3a807a25327
Jose Miguel Horcas Aguilera <jhorcas@us.es>
2021-07-15 13:42:14 +0200

    Refactor of core features operation

--- famapy/metamodels/fm_metamodel/operations/fm_core_features.py
+++ famapy/metamodels/fm_metamodel/operations/fm_core_features.py
@@ -4,36 +4,42 @@ from famapy.metamodels.fm_metamodel.models import FeatureModel, Feature
 
 
 class FMCoreFeatures(CoreFeatures):
-    """This implementation assumes that:
-     (1) there are not any dead-features in the model.
-     (2) there are not any false-optional features in the model.
+    """Core features are features that are present in all configurations of the feature model.
 
-     These assumptions imply that there are not any cross-tree constraints
-     involving the core-features, otherwise the model would contain
-     dead-features (for 'excludes' constraints) and false-optional features
-     (for 'requires' constraints).
-     """
+    This implementation assumes that:
+        (1) there are not any dead-features in the model.
+        (2) there are not any false-optional features in the model.
+
+    These assumptions imply that there are not any cross-tree constraints
+    involving the core features, otherwise the model would contain:
+        dead-features (e.g., for 'excludes' constraints) and 
+        false-optional features (e.g., for 'requires' constraints).
+    """
 
     def __init__(self):
         self.result = []
 
-    def get_core_features(self) -> list[Feature]:
+    def get_result(self) -> list[Feature]:
         return self.result
 
-    def get_result(self):
-        return self.get_core_features()
-
     def execute(self, model: FeatureModel) -> 'FMCoreFeatures':
-        if not model.root:  # void feature model
-            return self
-
-        core_features = [model.root]
-        features = [model.root]
-        while features:
-            feature = features.pop()
-            for relation in feature.get_relations():
-                if relation.is_mandatory():
-                    core_features.extend(relation.children)
-                    features.extend(relation.children)
-        self.result = core_features
+        self.result = get_core_features(model)
         return self
+
+    def get_core_features(self) -> list[Feature]:
+        return self.get_result()
+
+
+def get_core_features(feature_model: FeatureModel) -> list[Feature]:
+    if feature_model.root is None:
+        return []
+
+    core_features = [feature_model.root]
+    features = [feature_model.root]
+    while features:
+        feature = features.pop()
+        for relation in feature.get_relations():
+            if relation.is_mandatory():
+                core_features.extend(relation.children)
+                features.extend(relation.children)
+    return core_features
