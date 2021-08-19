37611bd887d5af196ba8fbbe96919ee1f67144da
pabpazjim <pablopazojim@gmail.com>
2021-08-19 13:54:22 +0200

    uvl tests clean

--- famapy/metamodels/fm_metamodel/transformations/test.py
+++
@@ -1,9 +0,0 @@
-from uvl_transformation import UVLTransformation
-from uvl_writter import UVLWriter
-
-uvl_transf = UVLTransformation("test.uvl")
-uvl_transf.transform()
-model = uvl_transf.model
-
-uvl_writer = UVLWriter(model, "writer_test.uvl")
-uvl_writer.transform()
