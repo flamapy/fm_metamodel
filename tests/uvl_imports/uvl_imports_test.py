01b98f56174d91fa7e7d795dee2271437c605bdc
pabpazjim <pablopazojim@gmail.com>
2021-10-15 12:40:54 +0200

    test tweaking

--- tests/uvl_imports/uvl_imports_test.py
+++ tests/uvl_imports/uvl_imports_test.py
@@ -4,21 +4,25 @@ from famapy.metamodels.fm_metamodel.models.feature_model import Constraint, Feat
 from famapy.metamodels.fm_metamodel.transformations.uvl_transformation import UVLTransformation
 
 
+def run(path) -> None:
+
+    raw_model_transformation = UVLTransformation(path, "raw_model.uvl")
+    raw_model_transformation.transform()
+    raw_model = raw_model_transformation.model
+    print(raw_model)
+
+    imports_model_transformation = UVLTransformation(path, "imports_model.uvl")
+    imports_model_transformation.transform()
+    imports_model = imports_model_transformation.model
+    print(imports_model)
+
+    assert(raw_model == imports_model)
+
+
 def normalize_path(path: str) -> str:
     return str(Path(__file__).parent.resolve()) + "/" + path
 
 
-def test_model_imports() -> None:
-    raw_model_path = normalize_path("models/raw_model.uvl")
-
-    raw_model_transformation = UVLTransformation(raw_model_path)
-    raw_model_transformation.transform()
-    raw_model = raw_model_transformation.model
-
-    imports_model_path = normalize_path("models/imports_model.uvl")
-
-    imports_model_transformation = UVLTransformation(imports_model_path)
-    imports_model_transformation.transform()
-    imports_model = imports_model_transformation.model
-
-    assert(raw_model == imports_model)
+def test_case_1() -> None:
+    path = normalize_path("models/case_1")
+    run(path)
