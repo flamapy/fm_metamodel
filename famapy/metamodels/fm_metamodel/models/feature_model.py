be1d89e84e0fe0c4c58c4df76a52ab9dd8344b96
José Miguel Horcas <jhorcas@us.es>
2022-02-14 16:23:26 +0100

    Refactor constraints into feature_model.py

--- famapy/metamodels/fm_metamodel/models/feature_model.py
+++ famapy/metamodels/fm_metamodel/models/feature_model.py
@@ -1,9 +1,7 @@
 from typing import Any, Optional
 from functools import total_ordering
 
-from famapy.core.models import VariabilityModel
-
-from famapy.metamodels.fm_metamodel.models import Constraint
+from famapy.core.models import AST, VariabilityModel
 
 
 class Relation:
@@ -177,6 +175,88 @@ class Feature:
         return str(self) < str(other)
 
 
+class Constraint:
+
+    def __init__(self, name: str, ast: AST):
+        self.name = name
+        self._ast = ast
+        self._clauses = self.ast.get_clauses()
+        if len(self._clauses) == 0:
+            raise ValueError(f'Error: wrong definition of constraint in AST: {ast}')
+        self._sorted_clauses = list(map(sorted, self._clauses))
+        self._sorted_clauses.sort(key=len)
+
+    @property
+    def ast(self) -> AST:
+        return self._ast
+
+    @ast.setter
+    def ast(self, ast: AST) -> None:
+        self._ast = ast
+        self._clauses = self.ast.get_clauses()
+        if len(self._clauses) == 0:
+            raise ValueError(f'Error: wrong definition of constraint in AST: {ast}')
+
+    def is_simple_constraint(self) -> bool:
+        return self.is_requires_constraint() or self.is_excludes_constraint()
+
+    def is_complex_constraint(self) -> bool:
+        return self.is_pseudocomplex_constraint() or self.is_strictcomplex_constraint()
+
+    def is_requires_constraint(self) -> bool:
+        if len(self._clauses) == 1 and len(self._clauses[0]) == 2:
+            nof_negative_clauses = sum(var.startswith('-') for var in self._clauses[0])
+            return nof_negative_clauses == 1
+        return False
+
+    def is_excludes_constraint(self) -> bool:
+        if len(self._clauses) == 1 and len(self._clauses[0]) == 2:
+            nof_negative_clauses = sum(var.startswith('-') for var in self._clauses[0])
+            return nof_negative_clauses == 2
+        return False
+
+    def is_strictcomplex_constraint(self) -> bool:
+        if len(self._clauses) == 1 and len(self._clauses[0]) == 2:
+            nof_negative_clauses = sum(var.startswith('-') for var in self._clauses[0])
+            return nof_negative_clauses == 0
+        strictcomplex = False
+        i = iter(self._clauses)
+        while not strictcomplex and (cls := next(i, None)) is not None:
+            if len(cls) != 2:
+                strictcomplex = True
+            else:
+                nof_negative_clauses = sum(var.startswith('-') for var in cls)
+                if nof_negative_clauses not in [1, 2]:
+                    strictcomplex = True
+        return strictcomplex
+
+    def is_pseudocomplex_constraint(self) -> bool:
+        if len(self._clauses) == 1:
+            return False
+        strictcomplex = False
+        i = iter(self._clauses)
+        while not strictcomplex and (cls := next(i, None)) is not None:
+            if len(cls) != 2:
+                strictcomplex = True
+            else:
+                nof_negative_clauses = sum(var.startswith('-') for var in cls)
+                if nof_negative_clauses not in [1, 2]:
+                    strictcomplex = True
+        return not strictcomplex
+
+    def __str__(self) -> str:
+        return f'({self.name}) {str(self.ast)}'
+
+    def __hash__(self) -> int:
+        return hash(str(self.ast).lower())
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Constraint) and str(self.ast).lower() == str(other.ast).lower()
+
+    def __lt__(self, other: Any) -> bool:
+        return str(self.ast).lower() < str(other.ast).lower()
+
+
 class FeatureModel(VariabilityModel):
 
     @staticmethod
@@ -186,7 +266,7 @@ class FeatureModel(VariabilityModel):
     def __init__(
         self,
         root: 'Feature',
-        constraints: Optional[list[Constraint]] = None
+        constraints: Optional[list['Constraint']] = None
     ) -> None:
         self.root = root
         self.ctcs = [] if constraints is None else constraints
@@ -211,25 +291,25 @@ class FeatureModel(VariabilityModel):
                 features.extend(relation.children)
         return features
 
-    def get_constraints(self) -> list[Constraint]:
+    def get_constraints(self) -> list['Constraint']:
         return self.ctcs
 
-    def get_simple_constraints(self) -> list[Constraint]:
+    def get_simple_constraints(self) -> list['Constraint']:
         return [ctc for ctc in self.ctcs if ctc.is_simple_constraint()]
 
-    def get_complex_constraints(self) -> list[Constraint]:
+    def get_complex_constraints(self) -> list['Constraint']:
         return [ctc for ctc in self.ctcs if ctc.is_complex_constraint()]
 
-    def get_requires_constraints(self) -> list[Constraint]:
+    def get_requires_constraints(self) -> list['Constraint']:
         return [ctc for ctc in self.ctcs if ctc.is_requires_constraint()]
 
-    def get_excludes_constraints(self) -> list[Constraint]:
+    def get_excludes_constraints(self) -> list['Constraint']:
         return [ctc for ctc in self.ctcs if ctc.is_excludes_constraint()]
 
-    def get_pseudocomplex_constraints(self) -> list[Constraint]:
+    def get_pseudocomplex_constraints(self) -> list['Constraint']:
         return [ctc for ctc in self.ctcs if ctc.is_pseudocomplex_constraint()]
 
-    def get_strictcomplex_constraints(self) -> list[Constraint]:
+    def get_strictcomplex_constraints(self) -> list['Constraint']:
         return [ctc for ctc in self.ctcs if ctc.is_strictcomplex_constraint()]
 
     def get_mandatory_features(self) -> list['Feature']:
