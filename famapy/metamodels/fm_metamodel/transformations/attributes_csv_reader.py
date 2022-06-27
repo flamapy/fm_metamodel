ba3e58db420d41d93bb06bed9ea25f7c5729ee2c
José Miguel Horcas <jhorcas@us.es>
2022-06-27 15:48:12 +0200

    Reader for attributes in CSV

---
+++ famapy/metamodels/fm_metamodel/transformations/attributes_csv_reader.py
@@ -0,0 +1,35 @@
+import csv
+
+from famapy.core.transformations import TextToModel
+from famapy.metamodels.fm_metamodel.models import (
+    Constraint,
+    Feature,
+    FeatureModel,
+    Relation,
+)
+
+
+class AttributesCSVReader(TextToModel):
+    """Reader for feature attributes in .csv.
+
+    The csv format is as follow:
+
+    Feature, Attribute1, Attribute2, Attribute3,...
+    featureAname, valueA1, valueA2, valueA3,... 
+    featureBname, valueB1, valueB2, valueB3,...
+    ...
+    """
+
+    @staticmethod
+    def get_source_extension() -> str:
+        return 'csv'
+
+    def __init__(self, path: str, model: FeatureModel) -> None:
+        self.path = path
+        self.model = model
+
+    def transform(self) -> FeatureModel:
+        with open(self.path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',', quotechar='"', skipinitialspace=True)
+            for row in reader:
+                print(row)
