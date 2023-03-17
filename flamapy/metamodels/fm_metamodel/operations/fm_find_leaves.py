2b4409cd828ea4af4ee0e37211da897dda4ebe74
joszamama <joszamama@gmail.com>
2023-03-17 11:22:46 +0100

    feat: add new operation for finding leaf features

---
+++ flamapy/metamodels/fm_metamodel/operations/fm_find_leaves.py
@@ -0,0 +1,27 @@
+from flamapy.core.operations import FindLeaves
+
+from flamapy.metamodels.fm_metamodel.models import FeatureModel
+from typing import Any
+
+class FMFindLeaves(FindLeaves):
+
+    def __init__(self) -> None:
+        self.result = 0
+
+    def get_result(self) -> list[Any]:
+        return self.result
+
+    def execute(self, model: FeatureModel) -> 'FMFindLeaves':
+        self.result = find_leaf_features(model)
+        return self
+
+    def get_leaves(self) -> list[Any]:
+        return self.get_result()
+
+
+def find_leaf_features(feature_model: FeatureModel) -> int:
+    leaves = []
+    for f in feature_model.get_features():
+        if f.is_leaf():
+            leaves.append(f)
+    return leaves
