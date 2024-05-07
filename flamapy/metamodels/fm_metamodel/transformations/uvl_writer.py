45b5b2cac96a5b11a4dff41e6f22d4fc74b2daa3
José Miguel Horcas <horcas@uma.es>
2024-05-07 18:56:00 +0200

    Remove quotes for numerical attributes

--- flamapy/metamodels/fm_metamodel/transformations/uvl_writer.py
+++ flamapy/metamodels/fm_metamodel/transformations/uvl_writer.py
@@ -58,7 +58,10 @@ class UVLWriter(ModelToText):
         for attribute in feature.get_attributes():
             attribute_str = attribute.name
             if attribute.default_value is not None:
-                attribute_str += f' "{attribute.default_value}"'
+                if isinstance(attribute.default_value, str):
+                    attribute_str += f" '{attribute.default_value}'"
+                else:
+                    attribute_str += f" {attribute.default_value}"
             attributes.append(attribute_str)
         return f'{{{", ".join(attributes)}}}' if attributes else ""
 
