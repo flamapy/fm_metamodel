cf6d912f4ad6ecadff5007b84082409e45e2e78f
José Miguel Horcas <horcas@uma.es>
2024-05-07 18:56:42 +0200

    Considering numerical types for ranges

--- flamapy/metamodels/fm_metamodel/operations/fm_generate_random_attribute.py
+++ flamapy/metamodels/fm_metamodel/operations/fm_generate_random_attribute.py
@@ -22,6 +22,7 @@ class GenerateRandomAttribute(Operation):
         self.result: FeatureModel
         self._attribute_name: str = ''
         self._attribute_domain: Domain
+        self._only_leaf_features: bool = False
 
     def get_result(self) -> FeatureModel:
         return self.result
@@ -32,6 +33,9 @@ class GenerateRandomAttribute(Operation):
     def set_domain(self, attribute_domain: Domain) -> None:
         self._attribute_domain = attribute_domain
 
+    def set_only_leaf_features(self, only_leaf_features: bool) -> None:
+        self._only_leaf_features = only_leaf_features
+
     def execute(self, model: FeatureModel) -> 'GenerateRandomAttribute':
         if model is None:
             raise FlamaException("Invalid feature model.")
@@ -41,18 +45,21 @@ class GenerateRandomAttribute(Operation):
             raise FlamaException("Attribute's domain has not been provided.")
         self.result = generate_random_attribute_values(model, 
                                                        self._attribute_name, 
-                                                       self._attribute_domain)
+                                                       self._attribute_domain,
+                                                       self._only_leaf_features)
         return self
 
 
 def generate_random_attribute_values(feature_model: FeatureModel, 
                                      name: str,
-                                     domain: Domain) -> FeatureModel:
+                                     domain: Domain,
+                                     only_leaf_features: bool) -> FeatureModel:
     for feature in feature_model.get_features():
-        if not any(name == attr.name for attr in feature.get_attributes()):
-            random_value = get_random_value_from_domain(domain)
-            new_attribute = Attribute(name, domain, random_value)
-            feature.add_attribute(new_attribute)
+        if not only_leaf_features or only_leaf_features and feature.is_leaf():
+            if not any(name == attr.name for attr in feature.get_attributes()):
+                random_value = get_random_value_from_domain(domain)
+                new_attribute = Attribute(name, domain, random_value)
+                feature.add_attribute(new_attribute)
     return feature_model
 
 
@@ -80,11 +87,13 @@ def get_random_value_from_ranges(ranges: list[Range]) -> Any:
     NOTE: This is not uniform if there are more than one range.
     """
     random_range = random.choice(ranges)
-    if isinstance(random_range.min_value, int):
-        value = random.randint(random_range.min_value, random_range.max_value)
-    elif isinstance(random_range.min_value, float):
+    if isinstance(random_range.min_value, float) or isinstance(random_range.max_value, float):
         min_digits = str(random_range.min_value)[::-1].find('.')
         max_digits = str(random_range.max_value)[::-1].find('.')
         digits = max(min_digits, max_digits)
         value = round(random.uniform(random_range.min_value, random_range.max_value), digits)
+    elif isinstance(random_range.min_value, int) and isinstance(random_range.max_value, int):
+        value = random.randint(random_range.min_value, random_range.max_value)
+    else:
+        raise FlamaException(f"Invalid range for attribute: {ranges}")
     return value
