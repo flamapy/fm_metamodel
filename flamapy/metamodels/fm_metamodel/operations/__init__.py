ef0753aac1889012daafcc5e20da31812bf98b35
José Miguel Horcas <horcas@uma.es>
2024-05-10 19:05:05 +0200

    Remove useless method imports

--- flamapy/metamodels/fm_metamodel/operations/__init__.py
+++ flamapy/metamodels/fm_metamodel/operations/__init__.py
@@ -1,20 +1,22 @@
-from .fm_core_features import FMCoreFeatures, get_core_features
-from .fm_count_leafs import FMCountLeafs, count_leaf_features
-from .fm_leaf_features import FMLeafFeatures, get_leaf_features
-from .fm_average_branching_factor import FMAverageBranchingFactor, average_branching_factor
-from .fm_feature_ancestors import FMFeatureAncestors, get_feature_ancestors
-from .fm_max_depth_tree import FMMaxDepthTree, max_depth_tree
-from .fm_estimated_products_number import FMEstimatedProductsNumber, count_configurations
-from .fm_atomic_sets import FMAtomicSets, get_atomic_sets
+from .fm_core_features import FMCoreFeatures
+from .fm_count_leafs import FMCountLeafs
+from .fm_leaf_features import FMLeafFeatures
+from .fm_average_branching_factor import FMAverageBranchingFactor
+from .fm_feature_ancestors import FMFeatureAncestors
+from .fm_max_depth_tree import FMMaxDepthTree
+from .fm_estimated_products_number import FMEstimatedProductsNumber
+from .fm_atomic_sets import FMAtomicSets
 from .fm_metrics import FMMetrics
 from .fm_generate_random_attribute import GenerateRandomAttribute
 
-__all__ = ['FMCoreFeatures', 'get_core_features',
-           'FMCountLeafs', 'count_leaf_features',
-           'FMLeafFeatures', 'get_leaf_features',
-           'FMAverageBranchingFactor', 'average_branching_factor',
-           'FMFeatureAncestors', 'get_feature_ancestors',
-           'FMMaxDepthTree', 'max_depth_tree',
-           'FMEstimatedProductsNumber', 'count_configurations',
-           'FMAtomicSets', 'get_atomic_sets', 'FMMetrics',
+
+__all__ = ['FMCoreFeatures',
+           'FMCountLeafs',
+           'FMLeafFeatures',
+           'FMAverageBranchingFactor',
+           'FMFeatureAncestors',
+           'FMMaxDepthTree',
+           'FMEstimatedProductsNumber',
+           'FMAtomicSets',
+           'FMMetrics',
            'GenerateRandomAttribute']
