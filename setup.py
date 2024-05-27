73015b36b439e1e39c3ca32357aa4850bdfab30a
José A. Galindo <jagalindo@us.es>
2024-05-27 16:17:23 +0200

    fix: updating operations to adhere to new name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flamapy-fm",
-    version="1.6.0.dev0",
+    version="2.0.0.dev0",
     author="Flamapy",
     author_email="flamapy@us.es",
     description="flamapy-fm is a plugin to Flamapy module",
@@ -22,7 +22,7 @@ setuptools.setup(
     ],
     python_requires='>=3.9',
     install_requires=[
-        'flamapy~=1.7.0.dev0',
+        'flamapy-fw~=2.0.0.dev0',
         'uvlparser~=2.0.1',
         'afmparser~=1.0.3',
     ],
