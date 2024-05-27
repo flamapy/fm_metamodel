ebbb209f9964b480f6c25b71c80c949c4b09e284
José A. Galindo <jagalindo@us.es>
2024-05-27 23:01:34 +0200

    docs: version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flamapy-fm",
-    version="2.0.0.dev0",
+    version="2.0.0.dev1",
     author="Flamapy",
     author_email="flamapy@us.es",
     description="flamapy-fm is a plugin to Flamapy module",
@@ -22,7 +22,7 @@ setuptools.setup(
     ],
     python_requires='>=3.9',
     install_requires=[
-        'flamapy-fw~=2.0.0.dev0',
+        'flamapy-fw~=2.0.0.dev1',
         'uvlparser~=2.0.1',
         'afmparser~=1.0.3',
     ],
@@ -37,6 +37,6 @@ setuptools.setup(
         ]
     },
     dependency_links=[
-        'flamapy~=1.6.0.dev0',
+        'flamapy~=2.0.0.dev1',
     ],
 )
